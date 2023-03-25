689893c46133552c5b580d82ef588ab00d060991
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-25 05:40:04 -1200

    Welcome page

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using InventoryManagement1;
+
+namespace InventoryManagement
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Inventory problems");
+
+
+        }
+    }
+
+
+
+}
