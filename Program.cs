6edc215c8e7921a3cbdf623acf728422116d9111
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-25 17:31:22 -1200

    calculate stock price

--- Program.cs
+++ Program.cs
@@ -20,12 +20,12 @@ namespace InventoryManagement
 
             for (int i = 0; i < data.stock1.Count; i++)
             {
-                Console.WriteLine(data.stock1[i].name);
-                Console.WriteLine(data.stock1[i].weight);
+                Console.WriteLine(data.stock1[i].shareName);
+                Console.WriteLine(data.stock1[i].NumberShares);
                 Console.WriteLine(data.stock1[i].price);
 
-                int val = data.stock1[i].weight * data.stock1[i].price;
-                Console.WriteLine("The price of {0} for {1} stock is {2}Rs. ", data.stock1[i].name, data.stock1[i].weight, val);
+                int val = data.stock1[i].NumberShares * data.stock1[i].price;
+                Console.WriteLine("The price of {0} for {1} stock is {2}Rs. ", data.stock1[i].shareName, data.stock1[i].shareName, val);
             }
 
             Console.WriteLine("-------------------------------------");
@@ -37,12 +37,12 @@ namespace InventoryManagement
 
             for (int i = 0; i < data.stock2.Count; i++)
             {
-                Console.WriteLine(data.stock2[i].name);
-                Console.WriteLine(data.stock2[i].weight);
+                Console.WriteLine(data.stock2[i].shareName);
+                Console.WriteLine(data.stock2[i].NumberShares);
                 Console.WriteLine(data.stock2[i].price);
 
-                int val = data.stock2[i].weight * data.stock2[i].price;
-                Console.WriteLine("The price of {0} for {1} stock is {2}Rs. ", data.stock2[i].name, data.stock2[i].weight, val);
+                int val = data.stock2[i].NumberShares * data.stock2[i].price;
+                Console.WriteLine("The price of {0} for {1} stock is {2}Rs. ", data.stock2[i].shareName, data.stock2[i].NumberShares, val);
             }
 
             Console.WriteLine("-------------------------------------");
@@ -54,12 +54,12 @@ namespace InventoryManagement
 
             for (int i = 0; i < data.stock3.Count; i++)
             {
-                Console.WriteLine(data.stock3[i].name);
-                Console.WriteLine(data.stock3[i].weight);
+                Console.WriteLine(data.stock3[i].shareName);
+                Console.WriteLine(data.stock3[i].NumberShares);
                 Console.WriteLine(data.stock3[i].price);
 
-                int val = data.stock3[i].weight * data.stock3[i].price;
-                Console.WriteLine("The price of {0} for {1} stock is {2}Rs. ", data.stock3[i].name, data.stock3[i].weight, val);
+                int val = data.stock3[i].NumberShares * data.stock3[i].price;
+                Console.WriteLine("The price of {0} for {1} stock is {2}Rs. ", data.stock3[i].shareName, data.stock3[i].NumberShares, val);
             }
 
             Console.WriteLine("-------------------------------------");
